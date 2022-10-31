e3ce6bfb82197dced77a6f0f864b0b8e3e9640c2
KaziSagar <kazi.sagar@northsouth.edu>
2022-10-31 23:21:37 +0600

    initial commit

---
+++ src/test/java/Utils.java
@@ -0,0 +1,26 @@
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Date;
+
+public class Utils {
+    public static void explicitWaitForTheElement(WebDriver driver, WebElement webElement, int SECOND){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(SECOND));
+        wait.until(ExpectedConditions.visibilityOf(webElement));
+    }
+
+    public static void takeScreenshot(WebDriver driver) throws IOException {
+
+        File screenshotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        String time = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss-aa").format(new Date());
+        String fileWithPath = "./src/test/resources/screenshots/" + time + ".png";
+        File DestFile = new File(fileWithPath);
+        FileUtils.copyFile(screenshotFile, DestFile);
+    }
+}
